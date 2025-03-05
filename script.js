80b476ce3eec222ec2e055ad83e181074585ef04
Jeff Ripke <jripke74@gmail.com>
2025-03-05 09:29:09 -0800

    add script.js and code

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const year = 2000;
 
 function isLeapYear(num) {
-  return num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0) ? `${num} is a leap year.` : `${num} is not a leap year.` 
+  return num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0)
+    ? `${num} is a leap year.`
+    : `${num} is not a leap year.`;
 }
 
 const result = isLeapYear(year);
